186641c71fbddf04d79f34e988aa4673ab8b76a0
oblind <oblind@163.com>
2019-02-13 17:02:48 +0800

    remove sub class

--- src/Language.php
+++ src/Language.php
@@ -4,53 +4,53 @@ namespace Oblind;
 require __DIR__ . '/global.php';
 
 class Language implements \ArrayAccess {
+  /**@var array $langs */
   static $langs = [];
+  /**@var static $lang */
   static $lang;
-  protected static $values = [];
+  protected $values = [];
 
   function offsetExists($offset): boolean {
-    return isset(static::$values[$offset]);
+    return isset($this->values[$offset]);
   }
 
   function offsetGet($offset) {
-    return static::$values[$offset] ?? $offset;
+    return $this->values[$offset] ?? $offset;
   }
 
   function offsetSet($offset, $value) {
-    static::$values[$offset] = $value;
+    $this->values[$offset] = $value;
   }
 
   function offsetUnset($offset) {
-    unset(static::$values[$offset]);
+    unset($this->values[$offset]);
   }
 
   /**
    * 设置语言
    *
-   * @param string|Language $lang
+   * @param string $lang
    * @return Language
    */
-  static function set($lang): Language {
-    if($lang instanceof Language)
-      $lang = get_class($lang);
-    else
-      $lang = str_replace(' ', '', ucwords(str_replace('-', ' ', $lang)));
+  static function set(string $lang): Language {
     if(!$l = static::$langs[$lang] ?? null) {
-      if(class_exists($lang)) {
-        $l = new $lang;
-        static::$langs[$lang] = $l;
-      } else {
-        if(!$l = static::$langs['en'] ?? null) {
-          $l = new Language;
-          static::$langs['en'] = $l;
-        }
-      }
+      $l = new static;
+      static::$langs[$lang] = $l;
     }
     static::$lang = $l;
     return $l;
   }
 
-  static function addTranslation($trans) {
-    static::$values = array_merge(static::$values, $trans);
+  static function addTranslation(array $trans, string $lang = null) {
+    if($lang) {
+      if(!$l = static::$langs[$lang] ?? null) {
+        $l = new static;
+        static::$langs[$lang] = $l;
+      }
+    } else
+      $l = static::$lang;
+    $l->values = array_merge($l->values, $trans);
   }
 }
+
+Language::set('en');
--- test/test.php
+++ test/test.php
@@ -3,11 +3,13 @@ require '../src/Language.php';
 
 use Oblind\Language;
 
-class ZhCN extends Language {
-  protected static $values = [
-    'hello' => '你好',
-  ];
-}
-Language::set('zh-cn');
+Language::addTranslation([
+  'hello' => '你好',
+], 'zh-cn');
+Language::addTranslation([
+  'hello' => 'fuck',
+]);
 
+echo _('hello'), "\n";
+Language::set('zh-cn');
 echo _('hello'), "\n";
