7d92cf00de7b4019a40b84dabb8a16163f1020c6
oblind <oblind@163.com>
2023-05-08 10:31:56 +0800

    translation function accepts placeholders and arguments

--- src/global.php
+++ src/global.php
@@ -1,7 +1,11 @@
 <?php
 
 if(!function_exists('_')) {
-  function _(string $k) {
-    return Oblind\Language::$lang[$k];
+  function _(?string $k, array $args = null) {
+    $r = Oblind\Language::$lang[$k] ?? $k;
+    if($r && $args)
+      foreach($args as $k => $v)
+        $r = str_replace("{{$k}}", $v, $r);
+    return $r;
   }
 }
